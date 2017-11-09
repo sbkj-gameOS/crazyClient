e85f66757d2448715560c8686d443a73c772d02c
14768776836 <784700109@qq.com>
2017-11-09 19:53:02 +0800

    田帅
    分享功能完成

--- assets/module/hall/script/ShareWx.js
+++ assets/module/hall/script/ShareWx.js
@@ -2,24 +2,22 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
+        typeStatus:0,
     },
 
     // use this for initialization
     onLoad: function () {
-        console.log("window.location.href:"+window.location.href);
+        this.urlAppend = '';
+        if(this.typeStatus == 1){//游戏首页分享
+            this.urlAppend = '';
+            this.descName = "心缘长春棋牌";
+        }else if(this.typeStatus == 2){//游戏中点击分享传递房间号参数
+            this.urlAppend = '?roomNum='+cc.beimi.room;
+            this.descName = "加入房间："+cc.beimi.room+",开始游戏！";
+        }
+        console.log("this.urlAppend:"+this.urlAppend);
         cc.beimi.http.httpPost("/wxController/getWxConfig",{url:window.location.href}, this.sucess , this.error , this);
-    },//http://tssb.bizpartner.cn/main/?userId=7c277a826ce543bc947298015b00a36c
-      //http://tssb.bizpartner.cn/main/?userId=7c277a826ce543bc947298015b00a36c
+    },
     sucess:function(result,object){
         result = JSON.parse(result) ;
         console.log(result);
@@ -42,8 +40,8 @@ cc.Class({
             // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
             wx.onMenuShareAppMessage({
                 title: '心缘长春棋牌',
-                desc: '心缘长春棋牌',
-                link: 'http://game.bizpartner.cn/wxController/toWXAuth',
+                desc: object.descName,
+                link: 'http://game.bizpartner.cn/wxController/toWXAuth'+object.urlAppend,
                 imgUrl: '',
                 trigger: function (res) {
                     // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
@@ -62,8 +60,8 @@ cc.Class({
 
             // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
             wx.onMenuShareTimeline({
-                title: '心缘长春棋牌',
-                link: 'http://game.bizpartner.cn/wxController/toWXAuth',
+                title: object.descName,
+                link: 'http://game.bizpartner.cn/wxController/toWXAuth'+object.urlAppend,
                 imgUrl: '',
                 trigger: function (res) {
                     // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
