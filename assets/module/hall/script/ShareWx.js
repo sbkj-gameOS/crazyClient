03e3014bee7b5fcf66c0c04f323edf8d7117e58b
14768776836 <784700109@qq.com>
2017-11-10 14:52:25 +0800

    田帅
    录音功能

--- assets/module/hall/script/ShareWx.js
+++ assets/module/hall/script/ShareWx.js
@@ -3,20 +3,31 @@ cc.Class({
 
     properties: {
         typeStatus:0,
+        tape:cc.Button,
     },
 
     // use this for initialization
     onLoad: function () {
+        this.START = 0;
+        this.END = 0;
+        this.recordTimer = 0;
         this.urlAppend = '';
         if(this.typeStatus == 1){//游戏首页分享
             this.urlAppend = '';
             this.descName = "心缘长春棋牌";
         }else if(this.typeStatus == 2){//游戏中点击分享传递房间号参数
             this.urlAppend = '?roomNum='+cc.beimi.room;
-            this.descName = "加入房间："+cc.beimi.room+",开始游戏！";
+            this.descName = "加入房间:"+cc.beimi.room+",开始游戏！";
         }
+        //按下开始录音
+        this.tape.node.on('touchstart', this.touchstartClick, this);
+        //松手结束录音
+        this.tape.node.on('touchend', this.touchendClick, this);
+
         console.log("this.urlAppend:"+this.urlAppend);
         cc.beimi.http.httpPost("/wxController/getWxConfig",{url:window.location.href}, this.sucess , this.error , this);
+        
+
     },
     sucess:function(result,object){
         result = JSON.parse(result) ;
@@ -30,12 +41,24 @@ cc.Class({
                 jsApiList: [
                     'checkJsApi',
                     'onMenuShareTimeline',
-                    'onMenuShareAppMessage'
+                    'onMenuShareAppMessage',
+                    'startRecord', 
+                    'stopRecord',
+                    'translateVoice',
+                    'onVoicePlayEnd',
+                    'playVoice',
+                    'uploadVoice','downloadVoice'
                 ]
             });
-
-         // 2. 分享接口
+        
         wx.ready(function(){
+
+            //注册微信播放录音结束事件【一定要放在wx.ready函数内】
+            wx.onVoicePlayEnd({
+                success: function (res) {
+                    stopWave();
+                }
+            });
 
             // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
             wx.onMenuShareAppMessage({
@@ -79,10 +102,60 @@ cc.Class({
             });
         });
     },
-       error:function(object){
-       },
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
+   error:function(object){
+   },
+   //开始录音
+   touchstartClick: function (event) {
+        this.START = new Date().getTime();
 
-    // },
+        this.recordTimer = setTimeout(function(){
+            wx.startRecord({
+                success: function(){
+                    localStorage.rainAllowRecord = 'true';
+                },
+                cancel: function () {
+                    alert('用户拒绝授权录音');
+                }
+            });
+        },300);
+    },
+    //停止录音
+    touchendClick: function (event) {
+        this.END = new Date().getTime();
+        wx.stopRecord({
+          success: function (res) {
+            //录音上传到微信服务器
+            wx.uploadVoice({
+                localId: res.localId, // 需要上传的音频的本地ID，由stopRecord接口获得
+                isShowProgressTips: 1, // 默认为1，显示进度提示
+                success: function (res) {
+                    //复制微信服务器返回录音id
+                    cc.beimi.serverId = res.serverId;
+                }
+            });
+          },
+          fail: function (res) {
+            alert(JSON.stringify(res));
+          }
+        });
+    },
+    //播放语音
+    startClick:function(){
+        //下载语音
+        wx.downloadVoice({
+            serverId: cc.beimi.serverId, // 需要下载的音频的服务器端ID，由uploadVoice接口获得
+            isShowProgressTips: 1, // 默认为1，显示进度提示
+            success: function (res) {
+                wx.playVoice({
+                    localId: res.localId // 需要播放的音频的本地ID，由stopRecord接口获得
+                });
+            }
+        });
+
+
+
+        // wx.playVoice({
+        //     localId: res.localId // 需要播放的音频的本地ID，由stopRecord接口获得
+        // });
+    }
 });
