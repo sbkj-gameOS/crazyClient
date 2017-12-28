0c32e004b9cf9dab7023110968107f7ba5a2d23f
ljhandndsy <258409778@qq.com>
2017-12-29 03:38:27 +0800

    刘佳欢

--- assets/module/game/majiang/script/MajiangDataBind.js
+++ assets/module/game/majiang/script/MajiangDataBind.js
@@ -1178,6 +1178,7 @@ cc.Class({
             //其他玩家出牌   
             let temp = context.player(data.userid , context) ;
             let cardpanel  , cardprefab , deskcardpanel;
+            
             if(temp.tablepos == "right"){
                 for(var inx = 0 ; inx < context.right_panel.children.length ; inx++){
                     let right_temp = context.right_panel.children[inx].getComponent("SpecCards");
@@ -1222,7 +1223,9 @@ cc.Class({
             /**
              * 销毁其中一个对象
              */
-            cardpanel.children[cardpanel.children.length - 1].destroy();
+            if(!data.notSend){
+                cardpanel.children[cardpanel.children.length - 1].destroy();                
+            }
             // let desk_card = cc.instantiate(cardprefab);
             // let desk_script = desk_card.getComponent("DeskCards");
             // desk_script.init(data.card);
