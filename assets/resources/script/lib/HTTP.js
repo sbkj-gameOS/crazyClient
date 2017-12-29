205490fb9619d5b2d5cb3270d798148345dd642c
ljhandndsy <258409778@qq.com>
2017-12-29 21:19:44 +0800

    刘佳欢

--- assets/resources/script/lib/HTTP.js
+++ assets/resources/script/lib/HTTP.js
@@ -16,14 +16,15 @@ var HTTP = cc.Class({
     },
 
         statics: {//127.0.0.1:8188 192.168.0.101
-            // baseURL:"http://192.168.0.:8081",
-            // wsURL : "http://192.168.199.241:9082",
+            // baseURL:"http://game.daily.bizpartner.cn",
+            // wsURL : "http://game.daily.bizpartner.cn:9081",
             
             baseURL:"http://game.bizpartner.cn",
             wsURL : "http://game.bizpartner.cn:9081",
             authorization: null,
             httpGet: function (url , success , error , object) {
             var xhr = cc.loader.getXMLHttpRequest();
+            cc.beimi.url = HTTP.baseURL;
             
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4) {
